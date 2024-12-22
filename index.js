d5d8f421e02f13b430643238ad116b7439beee6f
Ariyan Yeamin <yeaminfoysal14@gmail.com>
2024-12-22 09:05:22 -0800

    getting indiviual data by id from database complete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -34,6 +34,13 @@ async function run() {
     app.get('/items', async(req, res) => {
       const cursor = itemCollection.find()
       const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.get('/items/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await itemCollection.findOne(query)
       res.send(result)
     })
 
